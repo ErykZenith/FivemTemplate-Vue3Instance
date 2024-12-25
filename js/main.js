3ff03077fce75cbe292246f342245ce77de75a1d
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2024-12-25 15:01:14 +0700

    25/12/24

---
+++ js/main.js
@@ -0,0 +1,33 @@
+import post from "./post/index.js";
+
+const app = Vue.createApp({
+    data() {
+        return {
+           msg:"Vue3"
+        };
+    },
+    methods: {
+        onListener({ data: {
+            action,
+            data
+        }}) {
+            switch (action) {
+                case "open":
+                    console.log(data);
+                    break;
+                default:
+                    break;
+            }
+        },
+    },
+    async mounted() {
+        const res = await post("setup")
+        console.log("setup : ",res);
+        window.addEventListener("message", this.onListener)
+    },
+    onUnmounted() {
+        window.removeEventListener(this.onListener)
+    }
+});
+
+app.mount('#app');
