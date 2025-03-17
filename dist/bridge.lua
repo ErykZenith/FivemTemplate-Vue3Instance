dbfa4410a884d05ddcff5fb76a39fcd329df04d0
ErykZenith <167299127+ErykZenith@users.noreply.github.com>
2025-03-17 19:57:59 +0700

    Update bridge.lua

--- dist/bridge.lua
+++ dist/bridge.lua
@@ -4,4 +4,4 @@ local function _mfr(fn) return Citizen.GetFunctionReference(fn)end
 iframe = {}
 iframe.__index = iframe
 iframe.js = setmetatable({},{ __index = function(_, k) return setmetatable({},{ __index = function(_, _k) return function(...) _in(0x78608acb, _ts(json.encode({k, _k, ...})), _r) end end }) end })
-function iframe:on(callbackType, callback)_in(0xc59b980c, _ts(callbackType), _mfr(callback))end
+function iframe.on(callbackType, callback)_in(0xc59b980c, _ts(callbackType), _mfr(callback))end
